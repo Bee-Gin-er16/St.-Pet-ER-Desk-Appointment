f0424ebcafb0cfebc0c377e71d9ce60d77175edf
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2022-09-17 23:28:55 +0800

    Dashboard pt1
    Dashboard sidebar templating. Hoping for the best

--- client.js
+++ client.js
@@ -15,8 +15,11 @@ class Client{
 function getdata(){
     let jname = document.getElementById("user-name").value;
     let jpass = document.getElementById("psw").value;
+    if(jpass != document.getElementById("psw-repeat").value | document.getElementById("psw-repeat").innerHTML.length === 0){
+        console.log("Confirm password not the same or is empty");
+    }
     const client1 = new Client(jname, jpass);
-    console.log(jname);
-    console.log(jpass);
+    // console.log(jname);
+    // console.log(jpass);
     console.log(client1.display());
 }
